f6cd4277a456d97786d902b5368de88fdea85e97
kemkemG0 <kenshin.jpsdeveloper@gmail.com>
2023-04-05 10:08:38 +0000

    update

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ use std::{
     process::{Command, ExitStatus, Stdio},
 };
 
+const DEFAULT_SHELL: &str = "/bin/bash";
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
@@ -22,22 +24,26 @@ fn main() {
 fn run(args: Vec<String>) {
     let images_base_path = "./assets/images";
     let image = &args[2];
-    let tar = format!("{}/{}/{}.tar.gz", images_base_path, image, image);
+    let tar = format!("{images_base_path}/{image}/{image}.tar.gz");
 
     if !Path::new(&tar).exists() {
         panic!("Tar file not found");
     }
 
     let cmd = if args.len() > 3 {
-        args[3].clone()
+        args.clone()
+            .into_iter()
+            .skip(3)
+            .collect::<Vec<_>>()
+            .join(" ")
     } else {
-        read_to_string(format!("{}/{}/{}-cmd", images_base_path, image, image))
+        read_to_string(format!("{images_base_path}/{image}/{image}-cmd"))
             .expect("Commane not found")
     };
 
     let container_path = create_container_path(image).expect("Error creating container path");
     un_tar(&tar, Path::new(container_path.as_str()));
-    chroot(Path::new(container_path.as_str()), &cmd).expect("Error running command");
+    unshare_chroot_mount(Path::new(container_path.as_str()), &cmd).expect("Error running command");
 }
 
 fn pull(args: Vec<String>) {
@@ -48,32 +54,47 @@ fn pull(args: Vec<String>) {
     }
 }
 
-fn execute_command(call: &str, arg: Option<&str>) -> std::io::Result<ExitStatus> {
-    let mut cmd = Command::new(call);
-    if let Some(a) = arg {
-        cmd.arg(a);
-    }
+fn execute_command(command: &str, args: Vec<&str>) -> std::io::Result<ExitStatus> {
+    let mut cmd = Command::new(command);
+
+    println!("command: {}\nargs:{:?}", command, args);
+
     cmd.stdin(Stdio::inherit())
+        .args(args)
         .stdout(Stdio::inherit())
         .stderr(Stdio::inherit())
         .spawn()
-        .unwrap_or_else(|e| panic!("Error: {:?}\n Command: {} execution failed", e, call))
+        .unwrap_or_else(|e| panic!("Error: {:?}\n Command: {command} execution failed", e))
         .wait()
 }
 
 fn pull_image(image: &str) -> std::io::Result<ExitStatus> {
-    println!("Pulling image ....{}", image);
-    execute_command("./src/pull", Some(image))
+    println!("Pulling image ....{image}");
+    execute_command("./src/pull", vec![image])
 }
 
-fn chroot(new_root: &Path, call: &str) -> std::io::Result<ExitStatus> {
-    println!("Running {} in {:?}", call, new_root);
+fn exec_shell_and_mount_proc_and_exec_command(command: &str) -> std::io::Result<ExitStatus> {
+    let arg = format!("mount -t proc proc /proc && {DEFAULT_SHELL} -c '{command}'");
+    execute_command(DEFAULT_SHELL, vec!["-c", &arg])
+}
 
-    match nix::unistd::chroot(new_root) {
+fn chroot(new_root: &Path, command: &str) -> std::io::Result<ExitStatus> {
+    println!("Running {} in {:?}", command, new_root);
+    nix::unistd::fchdir(std::os::fd::AsRawFd::as_raw_fd(&File::open(new_root)?))?;
+
+    match nix::unistd::chroot(".") {
         Ok(_) => println!("chroot success"),
-        Err(e) => panic!("chroot failed: {}", e),
+        Err(e) => panic!("chroot failed: {e}"),
     }
-    execute_command(call, None)
+    exec_shell_and_mount_proc_and_exec_command(command)
+}
+
+fn unshare_chroot_mount(new_root: &Path, command: &str) -> std::io::Result<ExitStatus> {
+    // // Unshare the mount, PID, and network namespaces
+    use nix::sched::{unshare, CloneFlags};
+    unshare(CloneFlags::CLONE_NEWNS | CloneFlags::CLONE_NEWPID | CloneFlags::CLONE_NEWNET)
+        .expect("Failed to unshare namespaces");
+    chroot(Path::new(new_root), command)
 }
 
 fn create_container_path(image_name: &str) -> Result<String, Box<dyn Error>> {
@@ -85,7 +106,7 @@ fn create_container_path(image_name: &str) -> Result<String, Box<dyn Error>> {
 }
 
 fn un_tar(source: &str, dst: &Path) {
-    println!("Unpacking {} to {:?} ...{}", source, dst, dst.exists());
+    println!("Unpacking {source} to {:?} ...{}", dst, dst.exists());
     if dst.exists() {
         println!("Already unpacked");
         return;
